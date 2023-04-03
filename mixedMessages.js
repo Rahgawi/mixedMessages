c2990f12e5b4871489c9300d811967cdc830f6f6
rahgawirajakumaraswamy <rahgawiraja1999@outlook.de>
2023-04-03 12:35:03 +0200

    created a Array with Strings

---
+++ mixedMessages.js
@@ -0,0 +1,10 @@
+const quotes = ["Life isn’t about finding yourself. Life is about creating yourself" ,
+                "Your talent determines what you can do. Your motivation determines how much you’re willing to do. Your attitude determines how well you do it." ,
+                "The happiness of your life depends on the quality of your thoughts." ,
+                "Happiness is not something readymade. It comes from your own actions." ,
+                "You do not find the happy life. You make it." ,
+                "You learn more from failure than from success. Don’t let it stop you. Failure builds character." ,
+                "If there is no struggle, there is no progress." ,
+                "Develop success from failures. Discouragement and failure are two of the surest stepping stones to success." ,
+                "I’d rather regret the things I’ve done than regret the things I haven’t done"
+                ];
